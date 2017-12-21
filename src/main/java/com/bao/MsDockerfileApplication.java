0df11ed70df219719c72e1a22b609883d93f7798
wangfeishsh <baochunyu1987@163.com>
2017-12-21 10:58:51 +0800

    init

--- src/main/java/com/bao/MsDockerfileApplication.java
+++ src/main/java/com/bao/MsDockerfileApplication.java
@@ -2,9 +2,11 @@ package com.bao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @RestController
 @SpringBootApplication
 public class MsDockerfileApplication {
